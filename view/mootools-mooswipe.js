a558af9ca456b78079eb587ec8c59b6fa2d2387d
Yuri D'Elia <yuri.delia@eurac.edu>
2013-11-12 14:56:09 +0100

    Fix multi-touch events in mooswipe.
    Check for multi-touch events also in onTouchMove, and cancel motion if
    detected. This allows the user to perform normal multi-touch gestures, which
    would otherwise be prevented.

--- view/mootools-mooswipe.js
+++ view/mootools-mooswipe.js
@@ -1,6 +1,6 @@
 /*
 ---
-description: Left and right swipe events for touch devices.
+description: Swipe events for touch devices.
 
 license: MIT-style.
 
@@ -18,7 +18,7 @@ provides:
 */
 var MooSwipe = MooSwipe || new Class({
 	Implements: [Options, Events],
-	
+
 	options: {
 		//onSwipeleft: $empty,
 		//onSwiperight: $empty,
@@ -27,28 +27,29 @@ var MooSwipe = MooSwipe || new Class({
 		tolerance: 50,
 		preventDefaults: true
 	},
-	
+
 	element: null,
 	startX: null,
 	startY: null,
 	isMoving: false,
-	
+
 	initialize: function(el, options) {
 		this.setOptions(options);
 		this.element = $(el);
 		this.element.addListener('touchstart', this.onTouchStart.bind(this));
 	},
-	
+
 	cancelTouch: function() {
 		this.element.removeListener('touchmove', this.onTouchMove);
 		this.startX = null;
 		this.startY = null;
 		this.isMoving = false;
 	},
-	
+
 	onTouchMove: function(e) {
-		this.options.preventDefaults && e.preventDefault();
-		if (this.isMoving) {
+		if (e.touches.length == 1) {
+		    this.options.preventDefaults && e.preventDefault();
+		    if (this.isMoving) {
 			var dx = this.startX - e.touches[0].pageX;
 			var dy = this.startY - e.touches[0].pageY;
 			if (Math.abs(dx) >= this.options.tolerance) {
@@ -58,9 +59,13 @@ var MooSwipe = MooSwipe || new Class({
 				this.cancelTouch();
 				this.fireEvent(dy > 0 ? 'swipedown' : 'swipeup');
 			}
+		    }
+		}
+		else if (this.isMoving) {
+		  this.cancelTouch();
 		}
 	},
-	
+
 	onTouchStart: function(e) {
 		if (e.touches.length == 1) {
 			this.startX = e.touches[0].pageX;
